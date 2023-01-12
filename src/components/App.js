4ad2d5d7687a9b1a4ee7d2a3d8293153c4b46cb7
textshashank <shashankshukla1124@gmail.com>
2023-01-12 23:39:05 +0530

    shashank

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,10 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+      <h1 data-ns-test="project-name">My project</h1>
+      <p data-ns-test="project-description">is the best project</p>
+    </div>
   )
 }
 
